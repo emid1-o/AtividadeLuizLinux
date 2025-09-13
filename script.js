9194ba4896044479b3b595ee63e96e536faad8fb
emid1-o <emidiolop5@gmail.com>
2025-09-12 23:35:38 -0300

    finalizado

--- script.js
+++ script.js
@@ -2,6 +2,12 @@ const artistaInput = document.getElementById('artista');
 const botaoDeBusca = document.getElementById('buscarButton');
 botaoDeBusca.addEventListener('click', buscarAlbuns)
 const resultadosDiv = document.getElementById('resultados');
+const resultadosDivDois = document.getElementById('resultadosDois');
+const resultadosDivTres = document.getElementById('resultadosTres');
+const nomeUm = document.getElementById('nomeUm');
+const nomeDois = document.getElementById('nomeDois');
+const nomeTres = document.getElementById('nomeTres');
+
 
 
 async function buscarAlbuns() {
@@ -18,12 +24,22 @@ async function buscarAlbuns() {
     console.log("resposta da API recebida");
 
     const data = await response.json();
-    console.log("conversao pra JSON");
 
     const primeiroAlbum = data.topalbums.album[0].name;
+    const segundoAlbum = data.topalbums.album[1].name;
+    const terceiroAlbum = data.topalbums.album[2].name;
     const imagemAlbum = data.topalbums.album[0].image[2]['#text'];
+    const imagemAlbumDois = data.topalbums.album[1].image[2]['#text'];
+    const imagemAlbumTres = data.topalbums.album[2].image[2]['#text']
+
+    nomeUm.innerHTML = ` 1 - ${primeiroAlbum}`;
     resultadosDiv.innerHTML = `<img src="${imagemAlbum}">`;
-    console.log(`O album mais ouvido é: ${primeiroAlbum}`);
+    nomeDois.innerHTML = ` 2 - ${segundoAlbum}`;
+    resultadosDivDois.innerHTML = `<img src="${imagemAlbumDois}">`;
+    nomeTres.innerHTML = ` 3 - ${terceiroAlbum}`;
+    resultadosDivTres.innerHTML = `<img src="${imagemAlbumTres}">`;
+
+    console.log(`Os album mais ouvido são: ${primeiroAlbum, segundoAlbum, terceiroAlbum}`);
 
   } catch (error) {
 
